c89ec9644706b914096a73e1b323826935b9aa1e
EricC0419 <126376001+EricC0419@users.noreply.github.com>
2025-12-22 13:43:45 -0800

    Eric/get endpoints for jobs (#3)
    * add transcription field/column to jobs
    
    * boilerplate transcription function
    
    * jobs/id endpoint implementation
    
    * jobs/:id/result endpoint implementation
    
    * importing transcribe function ... should have been included in last pr
    
    * job/:id/result endpoint status codes correction
    
    ---------
    
    Co-authored-by: Eric Chang <er1c.chang@AirWicks-MCBK-3.local>

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -7,7 +7,7 @@ if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL is not set");
 }
 export const db = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL
 });
 
 /**
