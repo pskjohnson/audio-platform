fe760689f2b3f885d3b96156131e298c134391aa
Eric Chang <er1c.chang@AirWicks-MCBK-3.local>
2025-12-24 00:00:26 -0800

    fixed routes endpoints with json outputs on jobs.ts

--- apps/api/src/routes/jobs.test.ts
+++ apps/api/src/routes/jobs.test.ts
@@ -1,15 +1,12 @@
 import request from "supertest";
 import { app } from "../app";
 import { query } from "../db";
-import { transcribe } from "./services/transcription";
 
+const FAKE_ID = "899c14c9-6970-49d5-b93d-43921cd4dfb4";
 jest.mock("../db", () => ({
     query: jest.fn(),
   }));
 
-// jest.mock("../db", () => ({
-//     transcribe: jest.fn(),
-//   }));
 
 beforeEach(() => {
     jest.clearAllMocks();
@@ -17,19 +14,15 @@ beforeEach(() => {
 
 // this is for a file upload
 describe("POST /jobs", () => {
-    // this tests for when no file is uploaded ... error first
     it("returns 400 when no file is provided", async () => {
         const res = await request(app).post(`/jobs`)
         expect(res.status).toBe(400);
         expect(res.body).toEqual({error: "File is required"})
     });
-    // this tests for when a file is provided ... success later 
     it("returns 201 when a file is provided", async () => {
-        const fakeId = "899c14c9-6970-49d5-b93d-43921cd4dfb4";
-        (query as jest.Mock).mockResolvedValueOnce([{ id: fakeId, status: "queued" }]);
-        (query as jest.Mock).mockResolvedValueOnce([{ id: fakeId, status: "queued" }]);
-        (query as jest.Mock).mockResolvedValue([{ id: fakeId, status: "done" }]);
-        // (transcribe as jest.Mock).mockResolvedValueOnce(Promise<void>);
+        (query as jest.Mock).mockResolvedValueOnce([{ id: FAKE_ID, status: "queued" }]);
+        (query as jest.Mock).mockResolvedValueOnce([{ id: FAKE_ID, status: "queued" }]);
+        (query as jest.Mock).mockResolvedValue([{ id: FAKE_ID, status: "done" }]);
         const res = await request(app)
         .post("/jobs")
         .attach(
@@ -39,74 +32,65 @@ describe("POST /jobs", () => {
             );
         // Verify the mock was called
         expect(query).toHaveBeenCalledTimes(3);
-        
         expect(res.status).toBe(201);
         expect(res.body).toEqual({
-            id: fakeId,
-            status: "done"
+            jobId: FAKE_ID,
+            status: "queued"
         });
     });
 }); 
 
-// // this tests for an id get function
-describe("GET /jobs / :id", () => {
+describe("GET /jobs/:id", () => {
     it("returns 404 when an invalid id is entered", async () => {
         (query as jest.Mock).mockResolvedValueOnce([])
-        const fakeId = "899c14c9-6970-49d5-b93d-43921cd4dfb4";
-        const res = await request(app).get(`/jobs/${fakeId}`);
+        const res = await request(app).get(`/jobs/${FAKE_ID}`);
         expect(res.status).toBe(404);
         expect(res.body).toEqual({error: "job not found"});
     });
 
     it("returns 200 when valid id is entered", async () => {
-        const fakeId = "899c14c9-6970-49d5-b93d-43921cd4dfb4";
         (query as jest.Mock).mockResolvedValueOnce([{
-            id: fakeId,
+            id: FAKE_ID,
             status: "queued",
             created_at: "testTime1",
             updated_at: "testTime2",
         }]);
-        const res = await request(app).get(`/jobs/${fakeId}`);
+        const res = await request(app).get(`/jobs/${FAKE_ID}`);
         expect(res.status).toBe(200);
         expect(res.body).toEqual({
-            id: fakeId, 
+            jobId: FAKE_ID, 
             status: "queued",
-            created_at: "testTime1",
-            updated_at: "testTime2"
+            createdAt: "testTime1",
+            updatedAt: "testTime2"
         });
     });
 });
 
-
-
 describe("GET /jobs/:id/result", () => {
     it("returns 404 when an non-existant id is entered", async () => {
-        const fakeId = "899c14c9-6970-49d5-b93d-43921cd4dfb4";
         (query as jest.Mock).mockResolvedValueOnce([])
-        const res = await request(app).get(`/jobs/${fakeId}/result`);
+        const res = await request(app).get(`/jobs/${FAKE_ID}/result`);
         expect(res.status).toBe(404);
         expect(res.body).toEqual({error: "job is not found"})
     });
 
     it ("returns 202 when a valid file is entered but not finished", async () => {
-        const fakeId = "899c14c9-6970-49d5-b93d-43921cd4dfb4";
         (query as jest.Mock).mockResolvedValueOnce([{
-            id: fakeId,
+            id: FAKE_ID,
             status: "queued"
     }]);
-        const res = await request(app).get(`/jobs/${fakeId}/result`);
+        const res = await request(app).get(`/jobs/${FAKE_ID}/result`);
         expect(res.status).toBe(202);
         expect(res.body).toEqual({error: "job found but not finished"});
     });
 
     it ("returns 200 when jobs is found and is done", async () => {
-        const fakeId = "899c14c9-6970-49d5-b93d-43921cd4dfb4";
         (query as jest.Mock).mockResolvedValueOnce([{
-            id: fakeId,
+            id: FAKE_ID,
             status: "done",
             transcription: "transcription test"
     }]);
-        const res = await request(app).get(`/jobs/${fakeId}/result`);
+        const res = await request(app).get(`/jobs/${FAKE_ID}/result`);
         expect(res.status).toBe(200);
         expect(res.body).toEqual({text: "transcription test"});
     })
