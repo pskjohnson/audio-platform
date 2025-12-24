bffcf8d0a0c2ec363241ef653f7a10b44c8c7219
Eric Chang <er1c.chang@AirWicks-MCBK-3.local>
2025-12-23 23:59:25 -0800

    removed unused spec.ts

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -4,8 +4,7 @@ const config: Config = {
     preset: "ts-jest",
     testEnvironment: "node",
     testMatch: [
-        "**/*.test.ts",
-        "**/*.spec.ts"
+        "**/*.test.ts"
     ],
     clearMocks: true
 };
