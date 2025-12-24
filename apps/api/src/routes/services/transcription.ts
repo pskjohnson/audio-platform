c7b22b471e74387bd4e214f4308f47eefb5d0620
Eric Chang <er1c.chang@AirWicks-MCBK-3.local>
2025-12-23 19:29:53 -0800

    transcribe function return to fix post

--- apps/api/src/routes/services/transcription.ts
+++ apps/api/src/routes/services/transcription.ts
@@ -1,6 +1,7 @@
 import { query } from "../../db";
+import { Job } from "../types";
 
-export async function transcribe(jobId: string) : Promise<void> {
+export async function transcribe(jobId: string) : Promise<Job> {
     const queryString = "UPDATE jobs SET status = 'processing' WHERE id = $1 RETURNING *"
     const queryValues = [jobId]
     const [preTranscriptionJob] = await query(queryString, queryValues)
@@ -15,4 +16,5 @@ export async function transcribe(jobId: string) : Promise<void> {
     if (postTranscriptionJob.length ===0){
         throw new Error("Job not updated")
     }
+    return postTranscriptionJob;
 }
